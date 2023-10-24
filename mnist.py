840aefaf058818d70df5a19cac9990b09eda0fdc
Rishabh Budhia <rishabhbudhia@Rishabhs-Air.lan>
2023-10-23 20:51:17 -0700

    Base model initialization

--- mnist.py
+++ mnist.py
@@ -9,6 +9,13 @@ from sklearn.utils import shuffle
 import ssl
 ssl._create_default_https_context = ssl._create_unverified_context
 
+
+""" 
+Observation
+Dimension of weight matrix: 10 x 784 (Current layer number of neurons x previous layer number of neurons)
+Dimension of activation: 41000 x 10  (Training Samples x current layer number of neurons)
+"""
+
 # Set random seed for reproducibility
 torch.manual_seed(30)
 random.seed(30)
@@ -36,20 +43,42 @@ Y_train = torch.tensor(Y_train, dtype=torch.long, device=device)
 X_dev = torch.tensor(X_dev, dtype=torch.float32, device=device)
 Y_dev = torch.tensor(Y_dev, dtype=torch.long, device=device)
 
+# hook
+activation = {}
+def getActivation(name):
+  # the hook signature
+  def hook(model, input, output):
+    activation[name] = output.detach()
+  return hook
+
 # Define a customizable neural network class
 class CustomNeuralNetwork(nn.Module):
     def __init__(self, layer_dims):
         super(CustomNeuralNetwork, self).__init__()
         self.layers = nn.ModuleList()
         for i in range(1, len(layer_dims)):
-            self.layers.append(nn.Linear(layer_dims[i - 1], layer_dims[i]))
+            self.layers.append(nn.Linear(layer_dims[i - 1], layer_dims[i],bias=False))
             if i != len(layer_dims) - 1:
-                self.layers.append(nn.ReLU())  # Apply ReLU activation except in the last layer
-
+                # Apply ReLU activation except in the last layer
+                self.layers.append(nn.ReLU())
+    
     def forward(self, x):
+        h1 = self.layers[0].register_forward_hook(getActivation('layers[0]'))
+        h2 = self.layers[2].register_forward_hook(getActivation('layers[2]'))
+        h3 = self.layers[4].register_forward_hook(getActivation('layers[4]'))
+
         for layer in self.layers:
             x = layer(x)
+
+        # print(activation)
+        # for key, item in activation.items():
+        #     print(key, item.size(dim=0))
+
+        h1.remove()
+        h2.remove()
+        h3.remove()
         return x
+
 
 # Define the architecture of the neural network (customize as needed)
 layer_dims = [784, 10, 10, 10]  # Example: 3 layers with 10 neurons each
@@ -74,10 +103,15 @@ def train(model, X, Y, optimizer, criterion, iterations):
             with torch.no_grad():
                 predictions = torch.argmax(outputs, dim=1).cpu().numpy()
                 accuracy = (predictions == Y.cpu().numpy()).mean()
-                print(f"Iteration: {i}, Loss: {loss.item()}, Accuracy: {accuracy * 100}%")
-
+                print(
+                    f"Iteration: {i}, Loss: {loss.item()}, Accuracy: {accuracy * 100}%")
+                
+    # for name, param in model.state_dict().items():
+    #     print(name, param.shape)
+    
+              
 st = time.time()
-train(model, X_train, Y_train, optimizer, criterion, 500)
+train(model, X_train, Y_train, optimizer, criterion, 5)
 et = time.time()
 
 print(et - st)

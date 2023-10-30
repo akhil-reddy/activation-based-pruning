f4ccbf7ecd53bc234111c2156e393f7fc4911331
Ronak Jain <ronakjain@Ronaks-MacBook-Air.local>
2023-10-29 20:20:51 -0700

    Minor changes

--- ranking.py
+++ ranking.py
@@ -1,17 +1,19 @@
 import random
 
+
 def getRandomScores(weightMatrix):
     networkScores = []
+    maxScoreinLayer = []
     for index in range(1, len(weightMatrix.keys())):
-        layerScores = []
-        for neuron_Num in range(1, weightMatrix['layers.'+str(2*index)+'.weight'].shape[1]+1):
-            layerScores.append(random.randint(1, 200))
+        #for neuron_Num in range(1, weightMatrix['layers.'+str(2*index)+'.weight'].shape[1]+1):
+            #layerScores.append(random.randint(1, 200))
+        layerScores = list(range(1, weightMatrix['layers.'+str(2*index)+'.weight'].shape[1]+1))
+        random.shuffle(layerScores)
         networkScores.append(layerScores)
-    
-    return networkScores
+        maxScoreinLayer.append(weightMatrix['layers.'+str(2*index)+'.weight'].shape[1])
 
-""" 
-            Normal  Special
-    Local 
-    Global
-"""
+    lastLayer =  [1]* weightMatrix['layers.'+str(2*index)+'.weight'].shape[0]
+    networkScores.append(lastLayer)
+    maxScoreinLayer.append(1)
+    
+    return networkScores, maxScoreinLayer

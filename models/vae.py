ee26ef9d4d2306db42addac5bd7ad4de199ecc33
Akhil K <blitzkrieg.akhil@gmail.com>
2023-11-20 17:06:13 -0800

    VAE changes

--- models/vae.py
+++ models/vae.py
@@ -1,110 +1,93 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
 import ssl
 
 ssl._create_default_https_context = ssl._create_unverified_context
 
-
+# VAE Model with CNN Encoder
 class VAE(nn.Module):
-
-    def __init__(self, input_dim=784, hidden_dim=400, latent_dim=200):
+    def __init__(self):
         super(VAE, self).__init__()
+        # CNN Encoder
+        self.conv1 = nn.Conv2d(3, 16, kernel_size=3, stride=2, padding=1)
+        self.conv2 = nn.Conv2d(16, 32, kernel_size=3, stride=2, padding=1)
+        self.fc1 = nn.Linear(32 * 8 * 8, 400)
+        self.fc21 = nn.Linear(400, 20)
+        self.fc22 = nn.Linear(400, 20)
 
-        # encoder
-        self.encoder = nn.Sequential(
-            nn.Linear(input_dim, hidden_dim),
-            nn.LeakyReLU(0.2),
-            nn.Linear(hidden_dim, latent_dim),
-            nn.LeakyReLU(0.2)
-        )
-
-        # latent mean and variance
-        self.mean_layer = nn.Linear(latent_dim, 2)
-        self.logvar_layer = nn.Linear(latent_dim, 2)
-
-        # decoder
-        self.decoder = nn.Sequential(
-            nn.Linear(2, latent_dim),
-            nn.LeakyReLU(0.2),
-            nn.Linear(latent_dim, hidden_dim),
-            nn.LeakyReLU(0.2),
-            nn.Linear(hidden_dim, input_dim),
-            nn.Sigmoid()
-        )
+        # Decoder
+        self.fc3 = nn.Linear(20, 400)
+        self.fc4 = nn.Linear(400, 32 * 32 * 3)
 
     def encode(self, x):
-        x = self.encoder(x)
-        mean, logvar = self.mean_layer(x), self.logvar_layer(x)
-        return mean, logvar
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
+        x = x.view(x.size(0), -1)  # Flatten the feature maps
+        h1 = F.relu(self.fc1(x))
+        return self.fc21(h1), self.fc22(h1)
 
-    def reparameterization(self, mean, var):
-        epsilon = torch.randn_like(var)
-        z = mean + var * epsilon
-        return z
+    def reparameterize(self, mu, logvar):
+        std = torch.exp(0.5 * logvar)
+        eps = torch.randn_like(std)
+        return mu + eps * std
 
-    def decode(self, x):
-        return self.decoder(x)
+    def decode(self, z):
+
+        h3 = F.relu(self.fc3(z))
+        return torch.sigmoid(self.fc4(h3))
 
     def forward(self, x):
-        mean, log_var = self.encode(x)
-        z = self.reparameterization(mean, torch.exp(0.5 * log_var))
-        x_hat = self.decode(z)
-        return x_hat, mean, log_var
+        mu, logvar = self.encode(x)
+        z = self.reparameterize(mu, logvar)
+        return self.decode(z), mu, logvar
 
-    def prune_model_from_rankings(self,rankings, max_ranking, prune_percent=10):
-        layers = []
-        for layer_index in range(len(rankings)):
-            layer = []
-            for neuron_rank in rankings[layer_index]:
-                if neuron_rank > max(1, (1 - 0.01 * prune_percent) * max_ranking[layer_index]):
-                    layer.append(-1)
-                else:
-                    layer.append(1)
-            layers.append(layer)
-        return layers
 
-    '''
-    weights are from layer (0 to n] with n-1 elements
-    layers are from [1 to n] with n-1 elements
-    '''
+# Loss Function
+def loss_function(recon_x, x, mu, logvar):
+    BCE = F.binary_cross_entropy(recon_x, x.view(-1, 32 * 32 * 3), reduction='sum')
+    KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
+    return BCE + KLD
 
-    def reinit_model(self, weights, layers, device, input_layer):
-        layer_dims = [input_layer]
-        for layer in layers:
-            layer_dims.append(len([i for i in layer if i == 1]))
 
-        model = VAE(layer_dims).to(device)
-        for i in range(len(weights)):
-            weights[i] = torch.Tensor.tolist(weights[i])
+# Data Loading for CIFAR-10
+transform = transforms.Compose([
+    transforms.Resize((32, 32)),
+    transforms.ToTensor(),
+])
 
-        new_weights = []
-        # Format new weights
-        for i in range(len(layers)):
-            layer_weights = []
-            curr_layer_pruned_neurons = []
-            for j in range(len(layers[i])):
-                neuron_weights = weights[i][j]
-                # Neuron to prune
-                if layers[i][j] == -1:
-                    if i + 1 != len(layers):
-                        curr_layer_pruned_neurons.append(j)
-                else:
-                    layer_weights.append(neuron_weights)
-            new_weights.append(layer_weights)
+# Load the CIFAR-10 dataset
+dataset = datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
+dataloader = DataLoader(dataset, batch_size=64, shuffle=True)
 
-            if i + 1 != len(layers):
-                # Live neurons
-                live = [x for x in list(range(len(layers[i]))) if x not in curr_layer_pruned_neurons]
-                for k in range(len(layers[i + 1])):
-                    live_outflow = []
-                    # Only weights from live neurons in the current layer
-                    for j in live:
-                        live_outflow.append(weights[i + 1][k][j])
-                    weights[i + 1][k] = live_outflow
+# Initialize Model and Optimizer
+model = VAE()
+optimizer = optim.Adam(model.parameters(), lr=1e-3)
 
-        # Reinitialize new weights to the network
-        with torch.no_grad():
-            for i in range(len(layers)):
-                model.layers[2 * i].weight = nn.Parameter(torch.FloatTensor(new_weights[i]))
+# Training Loop
+num_epochs = 10
+for epoch in range(num_epochs):
+    model.train()
+    train_loss = 0
+    for batch_idx, (data, _) in enumerate(dataloader):
+        data = data.view(data.size(0), 3, 32, 32)  # Reshape data for CNN
+        optimizer.zero_grad()
+        recon_batch, mu, logvar = model(data)
+        loss = loss_function(recon_batch, data, mu, logvar)
+        loss.backward()
+        train_loss += loss.item()
+        optimizer.step()
+    print(f'Epoch: {epoch}, Loss: {train_loss / len(dataloader.dataset)}')
 
-        return model
+# Save the trained model
+# torch.save(model.state_dict(), 'vae_cifar10.pth')
+
+# Generate new images (optional)
+# model.eval()
+# with torch.no_grad():
+#     sample = torch.randn(64, 20)
+#     sample = model.decode(sample).cpu()
+#     # Save or display sample images

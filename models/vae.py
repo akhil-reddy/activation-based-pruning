54c049e8229e42b1bacbef33136e8599b5c51a1c
Akhil K <blitzkrieg.akhil@gmail.com>
2023-11-22 12:22:19 -0800

    VAE changes with Conv Inverse

--- models/vae.py
+++ models/vae.py
@@ -1,93 +1,113 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 import torch.optim as optim
-from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
-import ssl
+from torchvision import datasets, transforms
+from torch.autograd import Variable
+import matplotlib.pyplot as plt
+import torchvision.utils as vutils
 
-ssl._create_default_https_context = ssl._create_unverified_context
-
-# VAE Model with CNN Encoder
+# Define a simple VAE model
 class VAE(nn.Module):
     def __init__(self):
         super(VAE, self).__init__()
-        # CNN Encoder
-        self.conv1 = nn.Conv2d(3, 16, kernel_size=3, stride=2, padding=1)
-        self.conv2 = nn.Conv2d(16, 32, kernel_size=3, stride=2, padding=1)
-        self.fc1 = nn.Linear(32 * 8 * 8, 400)
-        self.fc21 = nn.Linear(400, 20)
-        self.fc22 = nn.Linear(400, 20)
 
-        # Decoder
-        self.fc3 = nn.Linear(20, 400)
-        self.fc4 = nn.Linear(400, 32 * 32 * 3)
+        self.encoder = nn.Sequential(
+            nn.Conv2d(3, 32, kernel_size=4, stride=2, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(32, 64, kernel_size=4, stride=2, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(64, 128, kernel_size=4, stride=2, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(128, 256, kernel_size=4, stride=2, padding=1),
+            nn.ReLU(),
+            nn.Flatten()
+        )
 
-    def encode(self, x):
-        x = F.relu(self.conv1(x))
-        x = F.relu(self.conv2(x))
-        x = x.view(x.size(0), -1)  # Flatten the feature maps
-        h1 = F.relu(self.fc1(x))
-        return self.fc21(h1), self.fc22(h1)
+        self.fc_mu = nn.Linear(256 * 4 * 4, 100)
+        self.fc_logvar = nn.Linear(256 * 4 * 4, 100)
+
+        self.decoder = nn.Sequential(
+            nn.Linear(100, 256 * 4 * 4),
+            nn.ReLU(),
+            nn.Unflatten(1, (256, 4, 4)),
+            nn.ConvTranspose2d(256, 128, kernel_size=4, stride=2, padding=1),
+            nn.ReLU(),
+            nn.ConvTranspose2d(128, 64, kernel_size=4, stride=2, padding=1),
+            nn.ReLU(),
+            nn.ConvTranspose2d(64, 32, kernel_size=4, stride=2, padding=1),
+            nn.ReLU(),
+            nn.ConvTranspose2d(32, 3, kernel_size=4, stride=2, padding=1),
+            nn.Sigmoid()
+        )
 
     def reparameterize(self, mu, logvar):
         std = torch.exp(0.5 * logvar)
         eps = torch.randn_like(std)
         return mu + eps * std
 
-    def decode(self, z):
-
-        h3 = F.relu(self.fc3(z))
-        return torch.sigmoid(self.fc4(h3))
-
     def forward(self, x):
-        mu, logvar = self.encode(x)
+        x = self.encoder(x)
+        mu = self.fc_mu(x)
+        logvar = self.fc_logvar(x)
         z = self.reparameterize(mu, logvar)
-        return self.decode(z), mu, logvar
+        x_reconstructed = self.decoder(z)
+        return x_reconstructed, mu, logvar
 
-
-# Loss Function
+# Define the loss function for VAE
 def loss_function(recon_x, x, mu, logvar):
-    BCE = F.binary_cross_entropy(recon_x, x.view(-1, 32 * 32 * 3), reduction='sum')
+    BCE = nn.BCELoss(reduction='sum')(recon_x, x)
     KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
     return BCE + KLD
 
-
-# Data Loading for CIFAR-10
+# Set up data transformation and loader
 transform = transforms.Compose([
-    transforms.Resize((32, 32)),
+    transforms.Resize((64, 64)),
     transforms.ToTensor(),
 ])
 
-# Load the CIFAR-10 dataset
-dataset = datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
-dataloader = DataLoader(dataset, batch_size=64, shuffle=True)
+dataset = datasets.CelebA(root='path/to/celeba', split='all', transform=transform, download=True)
+dataloader = DataLoader(dataset, batch_size=64, shuffle=True, num_workers=4)
 
-# Initialize Model and Optimizer
-model = VAE()
-optimizer = optim.Adam(model.parameters(), lr=1e-3)
+# Initialize the VAE model and optimizer
+vae = VAE()
+optimizer = optim.Adam(vae.parameters(), lr=1e-3)
 
-# Training Loop
+# Training loop
 num_epochs = 10
 for epoch in range(num_epochs):
-    model.train()
-    train_loss = 0
-    for batch_idx, (data, _) in enumerate(dataloader):
-        data = data.view(data.size(0), 3, 32, 32)  # Reshape data for CNN
+    for batch_idx, data in enumerate(dataloader):
+        img, _ = data
+        img = Variable(img)
+
         optimizer.zero_grad()
-        recon_batch, mu, logvar = model(data)
-        loss = loss_function(recon_batch, data, mu, logvar)
+
+        recon_batch, mu, logvar = vae(img)
+        loss = loss_function(recon_batch, img, mu, logvar)
+
         loss.backward()
-        train_loss += loss.item()
         optimizer.step()
-    print(f'Epoch: {epoch}, Loss: {train_loss / len(dataloader.dataset)}')
+
+        if batch_idx % 100 == 0:
+            print('Epoch [{}/{}], Batch [{}/{}], Loss: {:.4f}'.format(
+                epoch+1, num_epochs, batch_idx+1, len(dataloader), loss.item()
+            ))
+
+            # Visualize original and reconstructed images
+            if batch_idx % 500 == 0:
+                with torch.no_grad():
+                    vae.eval()
+                    sample = img[:4]
+                    recon_sample, _, _ = vae(sample)
+
+                    comparison = torch.cat([sample, recon_sample])
+                    comparison = comparison.view(-1, 3, 64, 64)
+                    comparison = vutils.make_grid(comparison, nrow=4, padding=5, normalize=True)
+
+                    plt.imshow(comparison.permute(1, 2, 0).numpy())
+                    plt.show()
+
+                vae.train()
 
 # Save the trained model
-# torch.save(model.state_dict(), 'vae_cifar10.pth')
-
-# Generate new images (optional)
-# model.eval()
-# with torch.no_grad():
-#     sample = torch.randn(64, 20)
-#     sample = model.decode(sample).cpu()
-#     # Save or display sample images
+torch.save(vae.state_dict(), 'vae_celeba.pth')

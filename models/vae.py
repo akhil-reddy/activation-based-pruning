681c47cbf0ee3ff7e3b79d8c81187b1a08e99673
Akhil K <blitzkrieg.akhil@gmail.com>
2023-11-22 13:53:10 -0800

    VAE changes with added layers and Reinit

--- models/vae.py
+++ models/vae.py
@@ -30,8 +30,10 @@ class VAE(nn.Module):
         self.decoder = nn.Sequential(
             nn.Linear(100, 256 * 4 * 4),
             nn.ReLU(),
-            nn.Unflatten(1, (256, 4, 4)),
-            nn.ConvTranspose2d(256, 128, kernel_size=4, stride=2, padding=1),
+            nn.Linear(256 * 4 * 4, 128 * 4 * 4),
+            nn.ReLU(),
+            nn.Unflatten(1, (128, 4, 4)),
+            nn.ConvTranspose2d(128, 128, kernel_size=4, stride=2, padding=1),
             nn.ReLU(),
             nn.ConvTranspose2d(128, 64, kernel_size=4, stride=2, padding=1),
             nn.ReLU(),
@@ -54,6 +56,65 @@ class VAE(nn.Module):
         x_reconstructed = self.decoder(z)
         return x_reconstructed, mu, logvar
 
+    def prune_model_from_rankings(self, rankings, max_ranking, prune_percent=10):
+        layers = []
+        for layer_index in range(len(rankings)):
+            layer = []
+            for neuron_rank in rankings[layer_index]:
+                if neuron_rank > max(1, (1 - 0.01 * prune_percent) * max_ranking[layer_index]):
+                    layer.append(-1)
+                else:
+                    layer.append(1)
+            layers.append(layer)
+        return layers
+
+    '''
+
+    weights are from layer (0 to n] with n-1 elements
+    layers are from [1 to n] with n-1 elements
+    '''
+
+    def reinit_model(self, weights, layers, device, input_layer):
+        layer_dims = [input_layer]
+        for layer in layers:
+            layer_dims.append(len([i for i in layer if i == 1]))
+
+        model = VAE().to(device)
+        for i in range(len(weights)):
+            weights[i] = torch.Tensor.tolist(weights[i])
+
+        new_weights = []
+        # Format new weights
+        for i in range(len(layers)):
+            layer_weights = []
+            curr_layer_pruned_neurons = []
+            for j in range(len(layers[i])):
+                neuron_weights = weights[i][j]
+                # Neuron to prune
+                if layers[i][j] == -1:
+                    if i + 1 != len(layers):
+                        curr_layer_pruned_neurons.append(j)
+                else:
+                    layer_weights.append(neuron_weights)
+            new_weights.append(layer_weights)
+
+            if i + 1 != len(layers):
+                # Live neurons
+                live = [x for x in list(range(len(layers[i]))) if x not in curr_layer_pruned_neurons]
+                for k in range(len(layers[i + 1])):
+                    live_outflow = []
+                    # Only weights from live neurons in the current layer
+                    for j in live:
+                        live_outflow.append(weights[i + 1][k][j])
+                    weights[i + 1][k] = live_outflow
+
+        # Reinitialize new weights to the network
+        with torch.no_grad():
+            for i in range(len(layers)):
+                model.layers[2 * i].weight = nn.Parameter(torch.FloatTensor(new_weights[i]))
+
+        return model
+
 # Define the loss function for VAE
 def loss_function(recon_x, x, mu, logvar):
     BCE = nn.BCELoss(reduction='sum')(recon_x, x)
@@ -66,7 +127,7 @@ transform = transforms.Compose([
     transforms.ToTensor(),
 ])
 
-dataset = datasets.CelebA(root='path/to/celeba', split='all', transform=transform, download=True)
+dataset = datasets.CelebA(root='data/celeba', split='all', transform=transform, download=False)
 dataloader = DataLoader(dataset, batch_size=64, shuffle=True, num_workers=4)
 
 # Initialize the VAE model and optimizer

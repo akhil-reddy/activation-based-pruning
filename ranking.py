34792a88e1bde0b49675ef783f56759e94b75b09
Akhil K <blitzkrieg.akhil@gmail.com>
2023-11-28 00:55:50 -0800

    Bug fixes in Reinit

--- ranking.py
+++ ranking.py
@@ -6,17 +6,14 @@ from collections import defaultdict
 def getRandomScores(weightMatrix):
     networkScores = []
     maxScoreinLayer = []
-    for index in range(1, len(weightMatrix.keys())):
+    what_i_need = ['encoder.5.weight', 'encoder.7.weight', 'decoder.0.weight', 'decoder.2.weight', 'decoder.4.weight']
+    for index in (what_i_need):
         # for neuron_Num in range(1, weightMatrix['layers.'+str(2*index)+'.weight'].shape[1]+1):
         # layerScores.append(random.randint(1, 200))
-        layerScores = list(range(1, weightMatrix['layers.' + str(2 * index) + '.weight'].shape[1] + 1))
+        layerScores = list(range(1, weightMatrix[index].shape[1] + 1))
         random.shuffle(layerScores)
         networkScores.append(layerScores)
-        maxScoreinLayer.append(weightMatrix['layers.' + str(2 * index) + '.weight'].shape[1])
-
-    lastLayer = [1] * weightMatrix['layers.' + str(2 * index) + '.weight'].shape[0]
-    networkScores.append(lastLayer)
-    maxScoreinLayer.append(1)
+        maxScoreinLayer.append(weightMatrix[index].shape[1])
 
     return networkScores, maxScoreinLayer
 
